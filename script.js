43ac03cca71c621be0b70e4248851d98b135032f
Ashon Woodbury <a.woodbury01@gmail.com>
2020-03-19 18:59:10 -0400

    Created objects

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ var lastName = "Woodbury";
 var age = 31;
 var likesTravel = true;
 
-console.log(firstName lastName age likesTravel);
+console.log(firstName, lastName, age, likesTravel);
+
+var ashonObject = {
+    firstName: "Ashon",
+    lastName: "Woodbury",
+    age: 31,
+    likesToTravel: true,
+    movieArray = ["The Dark Knight", "Inglorious Bastards", "The Prestige"]
+}
+console.log(ashonObject.favoriteFood)
+console.log(ashonObject);
