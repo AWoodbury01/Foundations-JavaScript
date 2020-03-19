d2f5cf073cf8b623aaf930eb630cf140fd9f57c8
Ashon Woodbury <a.woodbury01@gmail.com>
2020-03-19 19:55:25 -0400

    Practice exercises 1 and 2

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
-var firstName = "Ashon";
-var lastName = "Woodbury";
-var age = 31;
-var likesTravel = true;
+// var firstName = "Ashon";
+// var lastName = "Woodbury";
+// var age = 31;
+// var likesTravel = true;
 
-console.log(firstName, lastName, age, likesTravel);
+// console.log(firstName, lastName, age, likesTravel);
 
-var ashonObject = {
-    firstName: "Ashon",
-    lastName: "Woodbury",
-    age: 31,
-    likesToTravel: true,
-    movieArray = ["The Dark Knight", "Inglorious Bastards", "The Prestige"]
+// var ashonObject = {
+//     firstName: "Ashon",
+//     lastName: "Woodbury",
+//     age: 31,
+//     likesToTravel: true,
+//     favoriteMovies: ["The Dark Knight", "Inglorious Bastards", "The Prestige"]
+// }
+// console.log(ashonObject);
+// console.log(ashonObject.favoriteMovies)
+
+
+// //Today's Weather
+// var temperature = 69;
+// var conditions = "overcast";
+// var sunriseTime = "7:31 AM";
+// var sunsetTime = "7:39 PM"
+
+// console.log(temperature, conditions, sunriseTime, sunsetTime);
+// console.log("It's currently", conditions, "and", temperature, "degrees. The sun rose this morning at", sunriseTime, "and will set at", sunsetTime)
+
+
+// //Weather data
+// var highTemperatures = [55, 57, 53, 47, 56, 50];
+// var lowTemperatures = [33, 30, 29, 35, 31, 32]; 
+
+// console.log(highTemperatures[0]);
+// console.log(lowTemperatures[5]);
+
+// var averageHigh = (highTemperatures[0] + highTemperatures[1] + highTemperatures[2] + highTemperatures[3] + highTemperatures[4] + highTemperatures[5])/6;
+
+// console.log(averageHigh);
+
+
+//Weather object
+var weatherObject = {
+    temperature: 69,
+    conditions: "overcast",
+    sunriseTime: "7:31 AM",
+    sunsetTime: "7:39 PM"
 }
-console.log(ashonObject.favoriteFood)
-console.log(ashonObject);
+
+console.log(weatherObject.temperature);
+
+console.log("It's currently", weatherObject.conditions, "and", weatherObject.temperature, "degrees. The sun rose this morning at", weatherObject.sunriseTime, "and will set at", weatherObject.sunsetTime);
+
